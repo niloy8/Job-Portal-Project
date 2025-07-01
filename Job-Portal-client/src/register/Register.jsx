a533cd9f1e7c5692f039e514ef78e759002aa910
niloy8 <niloybhuiyan688@gmail.com>
2025-07-01 10:02:21 +0600

    Firebase Project Created

--- Job-Portal-client/src/register/Register.jsx
+++ Job-Portal-client/src/register/Register.jsx
@@ -1,26 +1,42 @@
-
+import Lottie from "lottie-react";
+import animationLogin from "../../src/assets/Lottie/register.json"
 const Register = () => {
+    const handleUser = (e) => {
+        e.preventDefault()
+        const email = e.target.email.value
+        const password = e.target.password.value
+
+        const passwordRegex = /^(?=.*[A-Z])(?=.*\d).{6,}$/;
+
+        if (!passwordRegex.test(password)) {
+            alert(
+                "Password must be at least 6 characters long, contain at least one uppercase letter and one number."
+            );
+            return;
+        }
+
+        console.log(email, password);
+
+    };
     return (
         <div className="hero bg-base-200 min-h-screen">
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <div className="text-center lg:text-left">
-
-                    <p className="py-6">
-                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                        quasi. In deleniti eaque aut repudiandae et a id nisi.
-                    </p>
+                    <Lottie animationData={animationLogin} className="w-80" loop={true}></Lottie>
                 </div>
                 <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
                     <h1 className="text-5xl font-bold ml-8 mt-4">Register now!</h1>
                     <div className="card-body">
-                        <fieldset className="fieldset">
-                            <label className="label">Email</label>
-                            <input type="email" className="input" placeholder="Email" />
-                            <label className="label">Password</label>
-                            <input type="password" className="input" placeholder="Password" />
-                            <div><a className="link link-hover">Forgot password?</a></div>
-                            <button className="btn btn-neutral mt-4">Sign Up</button>
-                        </fieldset>
+                        <form onSubmit={handleUser}>
+                            <fieldset className="fieldset">
+                                <label className="label">Email</label>
+                                <input type="email" name="email" className="input" placeholder="Email" />
+                                <label className="label">Password</label>
+                                <input type="password" name="password" className="input" placeholder="Password" />
+                                <div><a className="link link-hover">Forgot password?</a></div>
+                                <input type="submit" className="btn btn-neutral mt-4" value="Sign Up"></input>
+                            </fieldset>
+                        </form>
                     </div>
                 </div>
             </div>
